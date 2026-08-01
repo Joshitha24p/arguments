54ecc1ad7e19e0711e2f7c721313091c58408bf3
Joshitha24p <joshitha.joshi2012@gmail.com>
2026-08-01 12:16:56 -0400

    Add files via upload

---
+++ bill calculator.py
@@ -0,0 +1,12 @@
+def total_calc(bill_amount, tip_percent):
+    total=bill_amount*(1+0.01*tip_percent)
+    total=round(total,2)
+    print (f"Please pay ${total}")
+
+total_calc(150,20)
+
+def total_calc2(bill_amount1,discount_percent):
+    total=bill_amount1*(1-0.01*discount_percent)
+    total=round(total,2)
+    print (f"please pay ${total}")
+total_calc2(150,20)
